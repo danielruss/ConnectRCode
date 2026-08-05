bb3916ca5fd05c5dbaa7ee018a4dd53d07e1f87a
Daniel Russ <druss@mail.nih.gov>
2026-08-05 11:55:59 -0400

    Registration BEFORE BQ was loaded. The registration was overwritten when BQ loaded

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,6 @@
 .onLoad <- function(libname,pkgname){
+  # Load bigrquery first so our SQL translation is registered afterward.
+  loadNamespace("bigrquery")
   register_bq_translation()
   register_duckdb_translation()
 }
